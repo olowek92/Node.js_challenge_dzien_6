90c40165d6214829d1c577a6583db2d28ddf3931
Wojciech <olowek92@gmail.com>
2020-02-20 20:46:11 +0100

    Task-1 completed

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,20 @@
-//Twój kod
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("./public/zadanie01"));
+
+app.post("/result", (req, res) => {
+  const { first, second } = req.body;
+  let result = "";
+  if (parseInt(first) % parseInt(second) === 0) {
+    result = `${first} jest dzielnikiem ${second}`;
+  } else {
+    result = `${first} nie jest dzielnikiem ${second}`;
+  }
+  res.send(result);
+});
+
+app.listen(3800, (req, res) => {
+  console.log("Serwer uruchomiony na porcie 3800");
+});
